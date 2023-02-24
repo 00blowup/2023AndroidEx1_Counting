34051147dac55d54158539a5aae7517aac71e9b2
Haeun Noh <00blowup@gmail.com>
2023-02-25 03:34:22 +0900

    added log-generating event listeners

--- app/src/main/java/com/example/chapter2/MainActivity.kt
+++ app/src/main/java/com/example/chapter2/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.chapter2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val numberTextView =  findViewById<TextView>(R.id.numberTextView)
+        val resetButton = findViewById<Button>(R.id.resetButton)
+        val plusButton = findViewById<Button>(R.id.plusButton)
+
+        resetButton.setOnClickListener{
+            Log.d("onclick","리셋 버튼이 클릭되었습니다.")
+        }
+
+        plusButton.setOnClickListener{
+            Log.i("onclick","플러스 버튼이 클릭되었습니다.")
+        }
     }
 }
